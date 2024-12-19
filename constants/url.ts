b1e81c32cfa5d933eec99add183f303609272c0c
The Lu <luiseducol13@gmail.com>
2024-12-18 21:26:31 -0400

    new

--- constants/url.ts
+++
@@ -1,1 +0,0 @@
-export const BASE_URL_API = "https://5sf4jv49-3000.use2.devtunnels.ms";
--- utils/formatters.ts
+++
@@ -1,6 +0,0 @@
-export function NumberFormatter(num:Number){
-    return num.toLocaleString('es-ES', {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-      });
-}
